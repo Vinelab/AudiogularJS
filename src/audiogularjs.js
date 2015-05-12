dbcade0b99b7761adbdf505eea0f87ac55ef5ebe
joseph el alam <joseph.elalam@gmail.com>
2015-05-12 16:31:11 +0300

    initialize the project

---
+++ src/audiogularjs.js
@@ -0,0 +1,78 @@
+(function () {
+   'use strict';
+   angular.module("audiogularjs", []);
+   angular.module("audiogularjs").factory("AudiogularjsFactory", AudiogularjsFactory);
+   angular.module("audiogularjs").directive("audioPlay", audioPlay);
+
+   function AudiogularjsFactory() {
+      var data = {
+         audioObj: createAudio(),
+         playAudio: playAudio,
+         pauseAudio: pauseAudio,
+         resetAudio: resetAudio
+
+      };
+      return data;
+
+      function createAudio() {
+         return new Audio();
+      }
+      function playAudio(url) {
+         this.audioObj.src = url;
+         this.audioObj.play();
+      }
+      function pauseAudio() {
+         this.audioObj.pause();
+      }
+      function resetAudio() {
+         this.audioObj.load();
+      }
+   }
+
+
+   audioPlay.$inject = ['AudiogularjsFactory'];
+   function audioPlay(AudiogularjsFactory) {
+      return {
+         restrict: "EA",
+         scope: {
+            "src": "@"
+         },
+         replace: true,
+         require: "audioPlay",
+         template: '<div ng-class="audioPlayCtrl.isPlayed()"><div>',
+         bindToController: true,
+         controller: function () {
+            var self = this;
+            self.isPlayed = isPlayed;
+            self.playPause = playPause;
+
+            function isPlayed() {
+                  if (AudiogularjsFactory.audioObj.src === self.src && !AudiogularjsFactory.audioObj.paused) {
+                     return 'stopMe';
+                  } else {
+                     return 'playMe';
+                  }
+            }
+
+            function playPause() {
+               if (AudiogularjsFactory.audioObj.src === self.src && !AudiogularjsFactory.audioObj.paused) {
+                  AudiogularjsFactory.pauseAudio();
+               } else {
+                  AudiogularjsFactory.pauseAudio();
+                  AudiogularjsFactory.playAudio(self.src);
+               }
+            }
+         },
+         controllerAs: 'audioPlayCtrl',
+         link: function (scope, element, attrs, ctrls) {
+            element.on("click", function () {
+               ctrls.playPause();
+               scope.$apply();
+            });
+            scope.$on('$destroy', function () {
+               AudiogularjsFactory.resetAudio();
+            });
+         }
+      };
+   }
+})();
