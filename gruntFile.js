4886577b895a8f90839c40a896bbd6dbe3a321b6
joseph el alam <joseph.elalam@gmail.com>
2015-05-13 11:41:39 +0300

    add grunt-typescript and use it

--- gruntFile.js
+++ gruntFile.js
@@ -7,6 +7,15 @@ module.exports = function (grunt) {
          options: {
             port: 9000
          }
+      },
+      typescript: {
+         base: {
+            src: ['app/**/*.ts'],
+            dest: 'src/audiogularjs.js',
+            options: {
+               watch: true
+            }
+         }
       }
    });
 };
