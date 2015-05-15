2710b72e84f0cb6dfd488f8c4b56e918a1138b1e
joseph el alam <joseph.elalam@gmail.com>
2015-05-15 13:40:21 +0300

    add concat

--- gruntFile.js
+++ gruntFile.js
@@ -1,43 +1,43 @@
 module.exports = function (grunt) {
-   /* automatically load grunt tasks  */
-   require('load-grunt-tasks')(grunt);
-   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+    /* automatically load grunt tasks  */
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-      /* serve task to run the serve on 9000*/
-      serve: {
-         options: {
-            port: 9000
-         }
-      },
-
-      /* concat all the es6 files and put in temp/app.js */
-      concat: {
-         js: {
-            files: {
-               'temp/app.js': ['app/app.module.js', 'app/classes/*.js', 'app/**/*.js']
+        /* serve task to run the serve on 9000*/
+        serve: {
+            options: {
+                port: 9000
             }
-         }
-      },
+        },
 
-      /* convert temp/app.js to es5 on src/audiogularjs.js and create source map*/
-      babel: {
-         options: {
-            sourceMap: true
-         },
-         dist: {
-            files: {
-               'src/audiogularjs.js': 'temp/app.js'
+        /* concat all the es6 files and put in temp/app.js */
+        concat: {
+            js: {
+                files: {
+                    'temp/app.js': ['app/app.module.js', 'app/classes/audiogularjs.class.js', 'app/classes/*.js', 'app/**/*.js']
+                }
             }
-         }
-      },
+        },
 
-      /* watch changes on js files in  app directory and run the tasks : concat - babel above*/
-      watch: {
-         files: ['app/**/*.js'],
-         tasks: ['concat', 'babel']
-      }
+        /* convert temp/app.js to es5 on src/audiogularjs.js and create source map*/
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'src/audiogularjs.js': 'temp/app.js'
+                }
+            }
+        },
 
-   });
-   grunt.registerTask('default', 'concat files from app and store in <temp> then transform to es5 and watch changes', ['concat', 'babel', 'watch']);
+        /* watch changes on js files in  app directory and run the tasks : concat - babel above*/
+        watch: {
+            files: ['app/**/*.js'],
+            tasks: ['concat', 'babel']
+        }
+
+    });
+    grunt.registerTask('default', 'concat files from app and store in <temp> then transform to es5 and watch changes', ['concat', 'babel', 'watch']);
 };
